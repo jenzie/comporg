f7dc33429f0df6129c4adb3207aa6bb792d5cd7e
Jenny Zhen <jxz6853@rit.edu>
2014-03-26 00:08:44 -0400

    Adding initial files.

---
+++ lab6/build.asm
@@ -0,0 +1,58 @@
+# File:		build.asm
+# Author:	K. Reek
+# Contributors:	P. White,
+#		W. Carithers,
+#		<<<YOUR NAME HERE>>>
+#
+# Description:	Binary tree building functions.
+#
+# Revisions:	$Log$
+
+
+	.text			# this is program code
+	.align 2		# instructions must be on word boundaries
+
+# 
+# Name:		add_elements
+#
+# Description:	loops through array of numbers, adding each (in order)
+#		to the tree
+#
+# Arguments:	a0 the address of the array
+#   		a1 the number of elements in the array
+#		a2 the address of the root pointer
+# Returns:	none
+#
+
+	.globl	add_elements
+	
+add_elements:
+	addi 	$sp, $sp, -16
+	sw 	$ra, 12($sp)
+	sw 	$s2, 8($sp)
+	sw 	$s1, 4($sp)
+	sw 	$s0, 0($sp)
+
+#***** BEGIN STUDENT CODE BLOCK 1 ***************************
+#
+# Insert your code to iterate through the array, calling build_tree
+# for each value in the array.  Remember that build_tree requires
+# two parameters:  the address of the variable which contains the
+# root pointer for the tree, and the number to be inserted.
+#
+#***** END STUDENT CODE BLOCK 1 *****************************
+
+add_done:
+
+	lw 	$ra, 12($sp)
+	lw 	$s2, 8($sp)
+	lw 	$s1, 4($sp)
+	lw 	$s0, 0($sp)
+	addi 	$sp, $sp, 16
+	jr 	$ra
+
+#***** BEGIN STUDENT CODE BLOCK 2 ***************************
+#
+# Put your build_tree subroutine here.
+#
+#***** END STUDENT CODE BLOCK 2 *****************************
---
+++ lab6/traverse.asm
@@ -0,0 +1,27 @@
+# File:		traverse_tree.asm
+# Author:	K. Reek
+# Contributors:	P. White,
+#		W. Carithers,
+#		<<<YOUR NAME HERE>>>
+#
+# Description:	Binary tree traversal functions.
+#
+# Revisions:	$Log$
+
+
+# CONSTANTS
+#
+
+# traversal codes
+PRE_ORDER  = 0
+IN_ORDER   = 1
+POST_ORDER = 2
+
+	.text			# this is program code
+	.align 2		# instructions must be on word boundaries
+
+#***** BEGIN STUDENT CODE BLOCK 3 *****************************
+#
+# Put your traverse_tree subroutine here.
+# 
+#***** END STUDENT CODE BLOCK 3 *****************************
