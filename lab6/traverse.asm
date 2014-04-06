0e96f572bf310929a2ed4e8661470bac202d4cd5
Jenny Zhen <jxz6853@rit.edu>
2014-04-06 16:57:19 -0400

    Adding code to perform pre-order, -in-order, and post-order traversals.

--- lab6/traverse.asm
+++ lab6/traverse.asm
@@ -27,8 +27,8 @@ POST_ORDER = 2
 
 traverse_tree:
 
-# Saving the previous function caller's s-registers (the stack).
-# So that the upcoming function's modifications does not lose old data.
+	# Saving the previous function caller's s-registers (the stack).
+	# So that the upcoming function's modifications does not lose old data.
 
 	addi	$sp, $sp, -20
 	sw	$s0, 0($sp)			# saving the top of stack
@@ -42,7 +42,7 @@ traverse_tree:
 	lw	$s1, 8($a0)					# a0's right node
 	
 	move $s2, $a0					# save the a0 node
-	move $s3, $a1					# save the type of traversal
+	move $s3, $a1					# save the print function
 	
 	li	$t0, PRE_ORDER				# save the flag for pre_order
 	li	$t1, IN_ORDER				# save the flag for in_order
@@ -52,16 +52,49 @@ traverse_tree:
 	beq $t1, $a2, in_order			# go to in_order traversal
 	beq $t2, $a2, post_order		# go to post_order traversal
 	
-# root, left, right
 pre_order:
+	# root, left, right
+	
 	move	$a0, $s2				# process root
-	
+	jalr	$s3						# go to function to print the node
+	move	$a0, $s0				# traverse left
+	jal	traverse_tree
+	move	$a0, $s1				# traverse right
+	j	traverse_done				# processed all nodes
 
-# left, root, right
 in_order:
+	# left, root, right
 	
-# left, right, root
+	move	$a0, $s0				# traverse left
+	jal	traverse_tree
+	move	$a0, $s2				# process root
+	jalr	$s3						# go to function to print the node
+	move	$a0, $s1				# traverse right
+	jal	traverse_tree
+	j	traverse_done				# processed all nodes
+
 post_order:
+	# left, right, root
 	
+	move	$a0, $s0				# traverse left
+	jal traverse_tree
+	move	$a0, $s1				# traverse right
+	jal	traverse_tree
+	move	$a0, $s2				# process root
+	jalr	$s3						# go to function to print the node
+	j	traverse_done				# processed all nodes
+	
+traverse_done:
+	
+	# Restore the previous function caller's s-registers.
+	# So that the modifications made from the traversals do not lose old data.
+	
+	sw	$s0, 0($sp)			# saving the top of stack
+	sw	$s1, 4($sp)
+	sw	$s2, 8($sp)
+	sw	$s3, 12($sp)		# saving the bottom of stack
+	sw	$ra, 16($sp)		# saving the return address of previous function
+	addi	$sp, $sp, 20	# undo the addi -20 from the beginning
+	jr	$ra					# return to the function caller
  
 #***** END STUDENT CODE BLOCK 3 *****************************
