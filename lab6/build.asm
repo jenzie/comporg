670bea7f074f73102db8890e0b49918e9f6b1bc5
Jenny Zhen <jxz6853@rit.edu>
2014-04-06 19:26:59 -0400

    Adding code to read the array of node values to build the tree.

--- lab6/build.asm
+++ lab6/build.asm
@@ -39,7 +39,27 @@ add_elements:
 # for each value in the array.  Remember that build_tree requires
 # two parameters:  the address of the variable which contains the
 # root pointer for the tree, and the number to be inserted.
-#
+
+	STOP_VAL = 9999		# stop reading node values when 9999 is read in
+	STOP_NUM = 20		# stop reading node values when 20 values were read
+	
+	li	$t0, 0			# counter for number of nodes added to the tree
+	move	$s0, $a0	# save the pointer to the array of node values to add
+	move	$s1, $a1	# save the number of elements left to be read
+	
+read_array_of_values:
+	lw	$t1, 0($s0)		# store the next value of a node to be added to the tree
+	beq	$t1, STOP_VAL, add_done		# stop if the value read in was 9999
+	addi	$s1, -1		# decrement the number of elements left to be read
+	addi	$t0, 1		# increment the number of nodes added to the tree
+	move	$a0, $a2	# set the pointer to the root node of the tree
+	move	$a1, $t1	# set the value of the node to be added to the tree
+	jal	build_tree
+	beq	$t0, STOP_NUM, add_done		# stop if 20 values were read
+	beq	$s1, $zero, add_done		# stop if 0 values left to be read
+	addi	$s0, 4		# move the pointer to the array of node values to add
+	j	read_array_of_values
+
 #***** END STUDENT CODE BLOCK 1 *****************************
 
 add_done:
