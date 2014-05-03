9e687070d526b4aa1def8a3e5c8d218e653e8a61
Jenny Zhen <jxz6853@rit.edu>
2014-05-03 17:30:31 -0400

    Adding input checking.

--- project/skyscrapers.asm
+++ project/skyscrapers.asm
@@ -8,7 +8,9 @@
 #
 
 # CONSTANTS
-#
+MIN_SIZE = 3
+MAX_SIZE = 8
+
 # syscall codes
 PRINT_INT = 1
 PRINT_STRING = 4
@@ -87,7 +89,7 @@ illegal_fv_input:
 	.asciiz "Illegal fixed input values, Skyscrapers terminating\n"
 	
 impossible_puzzle:
-	.asciiz "Impossible Puzzle\n"
+	.asciiz "Impossible Puzzle\n\n"
 	
 	.text			# this is program code
 	.align	2		# instructions must be on word boundaries
@@ -109,6 +111,8 @@ main:
 	syscall
 	
 	sw	board_size, $v0		# store the value of the first param, board size
+	blt	board_size, MIN_SIZE, error_board_size		# validate input
+	bgt	board_size, MIN_SIZE, error_board_size		# validate input
 	
 	la	$a0, north_array	# store the address of the pointer to north_array
 	jal	parse_board_perim	# parse the input for north
@@ -119,13 +123,49 @@ main:
 	la	$a0, west_array		# store the address of the pointer to west_array
 	jal	parse_board_perim	# parse the input for west
 	
+error_board_size:
+	li	$v0, PRINT_STRING			# load the syscall code
+	la	$a0, invalid_board_size		# load the address to the string
+	syscall							# tell the OS to print
+	sysexit							# terminate program
 	
+error_input_value:
+	li	$v0, PRINT_STRING			# load the syscall code
+	la	$a0, illegal_input			# load the address to the string
+	syscall							# tell the OS to print
+	sysexit							# terminate program
+	
+error_num_fv:
+	li	$v0, PRINT_STRING			# load the syscall code
+	la	$a0, invalid_num_fv			# load the address to the string
+	syscall							# tell the OS to print
+	sysexit							# terminate program
+	
+error_fv_input:
+	li	$v0, PRINT_STRING			# load the syscall code
+	la	$a0, illegal_fv_input		# load the address to the string
+	syscall							# tell the OS to print
+	sysexit							# terminate program
+	
+error_impossible puzzle:
+	li	$v0, PRINT_STRING			# load the syscall code
+	la	$a0, impossible_puzzle		# load the address to the string
+	syscall							# tell the OS to print
+	sysexit							# terminate program
+
 parse_board_perim:
-	li	$t0, 0				# counter for the number of values read in
+	li	$t0, 0						# counter for the number of values read in
 	
 pbp_loop:
-	li	$v0, READ_INT		# read in a single perimeter value
-	addi	$t0, 1			# increment counter
+	li	$v0, READ_INT				# read in a single perimeter value
+	syscall
+	
+	blt	$v0, $zero, error_input_value				# validate input
+	bgt	$v0, board_size, error_input_value			# validate input
+	
+	sw	$v0, 0($a0)					# store the perimeter value
+	addi	$a0, $a0, 4				# move address to base pointer over
+	addi	$t0, $t0, 1				# increment counter
 	beq	$t0, board_size, pbp_done
 	j	pbp_loop
 
