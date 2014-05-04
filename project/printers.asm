affd41fa73290d27881effb0ae879f82e94ad6f0
Jenny Zhen <jxz6853@rit.edu>
2014-05-04 15:59:05 -0400

    Adding code for finishing printing the board.

--- project/printers.asm
+++ project/printers.asm
@@ -38,12 +38,10 @@ print_board:
 	addi	$sp, $sp, -4			# save return address of the caller
 	sw	$ra, 0($sp)
 	
-	# print north
 	jal	print_north_row
-	
-	
-	
+	jal	print_west_east_rows
 	jal	print_south_row
+	
 									# restore for print_north_row
 	lw	$ra, 0($sp)					# restore return address of the caller
 	addi	$sp, $sp, 4
@@ -82,11 +80,91 @@ print_south_row:
 	
 	j	print_loop
 	
-print_east_west_row:
+print_west_east_rows:
+	la	$t1, west_array				# load the address of an element in west
+	lw	$t2, 0($t1)					# load the value of the element in west
 	
+	la	$t3, east_array				# load the address of an element in east
+	lw	$t4, 0($t3)					# load the value of the element in east
+	
+	la	$t5, board_array			# load the address of an element in board
+	lw	$t6, 0($t5)					# load the value of the element in board
+	
+	move	$t8, $zero				# count the number of rows printed
+	
+									# save for print_loop_s
+	addi	$sp, $sp, -4			# save return address of the caller
+	sw	$ra, 0($sp)
+	
+	j	print_we_loop
+	
+print_we_loop:
+	beq	$t8, $s7, print_we_done		# check counter for num rows
+	
+									# print west value
+	li 	$v0, PRINT_STRING			# load the syscall code
+	la 	$a0, $t2					# load the address to the string
+	syscall							# tell the OS to print
+	
+	li 	$v0, PRINT_STRING			# load the syscall code
+	la	$a0, space					# load the address to the string
+	syscall							# tell the OS to print
+	
+	move	$t7, $zero				# count the number of tiles printed
+	
+									# save for print_tile
+	addi	$sp, $sp, -4			# save return address of the caller
+	sw	$ra, 0($sp)
+	
+	jal	print_loop_s
+	
+	li 	$v0, PRINT_STRING			# load the syscall code
+	la	$a0, space					# load the address to the string
+	syscall							# tell the OS to print
+	
+									# print east value
+	li 	$v0, PRINT_STRING			# load the syscall code
+	la 	$a0, $t4					# load the address to the string
+	syscall							# tell the OS to print
+	
+	li 	$v0, PRINT_STRING			# load the syscall code
+	la	$a0, new_line				# load the address to the string
+	syscall							# tell the OS to print
+	
+print_we_done:
+									# restore for print_loop_s
+	lw	$ra, 0($sp)					# restore return address of the caller
+	addi	$sp, $sp, 4
+	
+	jr	$ra
+	
+print_loop_s:
+	beq	$t7, $s7, print_done_s		# check counter for num tiles
+	
+	li 	$v0, PRINT_STRING			# load the syscall code
+	la	$a0, single_col_separator	# load the address to the string
+	syscall							# tell the OS to print
+	
+	jal	print_tile
+	
+	addi	$t7, $t7, 1				# increment the counter
+	addi	$t5, $t5, 4				# move the base pointer over
+	lw	$t6, 0($t5)					# load the value of the tile
+	j	print_loop
+	
+print_done_s:
+									# restore for print_tile
+	lw	$ra, 0($sp)					# restore return address of the caller
+	addi	$sp, $sp, 4
+	
+	li 	$v0, PRINT_STRING			# load the syscall code
+	la	$a0, single_col_separator	# load the address to the string
+	syscall							# tell the OS to print
+	
+	jr	$ra
 	
 print_loop:
-	beq	$t7, $s7, print_done		# check counter
+	beq	$t7, $s7, print_done		# check counter for num tiles
 	
 	jal	print_tile
 	
@@ -151,13 +229,6 @@ print_one_divider:
 print_div_done:
 	li 	$v0, PRINT_STRING			# load the syscall code
 	la	$a0, row_terminator			# load the address to the string
-	syscall							# tell the OS to print
-	
-	jr	$ra
-	
-print_col_divider:
-	li 	$v0, PRINT_STRING			# load the syscall code
-	la	$a0, single_col_separator	# load the address to the string
 	syscall							# tell the OS to print
 	
 	jr	$ra
